84a4986dfec0b1b74731d5e927a797beaeee7160
Mustafa566 <50444816+Mustafa566@users.noreply.github.com>
2020-12-07 18:29:59 +0100

    test

--- ytBot.py
+++ ytBot.py
@@ -1,8 +1,12 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import json
+import replace
 
-srt = YouTubeTranscriptApi.get_transcript('lICIfpoD-jI')
+
+srt = YouTubeTranscriptApi.get_transcript('veMFCFyOwFI')
 
 with open('yt.txt', 'w') as file:
-    file.writelines('%s\n' % i for i in srt)
+    # file.writelines('%s\n' % i for i in srt)
+    json.dump(srt, file)
 
 print('done')
